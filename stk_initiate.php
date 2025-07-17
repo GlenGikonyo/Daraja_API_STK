8d6b92cfd18099c5087bbbaf1cdf96c28b0ff7b7
GlenGikonyo <gikonyoglen@gmail.com>
2025-07-17 16:20:22 +0300

    feat: add render configuration and update access tokens in stk_initiate.php

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,8 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'StdB1QWZxCTR7rxc7tST2yGDxfcGEEJJ2fSr58wA8nd1adBi'; //Fill with your app Consumer Key
+  $consumerSecret = '4j0h5g2Pr9BKUy41tv5bVUx5Oi6W23UKOqRkSlORjEhdLnAV6qKWdObmdXpKNIXD'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
